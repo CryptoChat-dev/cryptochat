1725a7363e1053fd8c334705666aa0d0d4324099
httpjamesm <github@httpjames.space>
2021-04-10 17:09:38 -0400

    fix /jquery route directory

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/aes', (req, res) => {
 })
 
 app.get('/jquery', (req, res) => {
-    res.sendFile('scripts/jquery.min.js')
+    res.sendFile('scripts/jquery.min.js', { root: __dirname })
 })
 
 // Icons
