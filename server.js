a98a58b76d5958871ad58f713f3def2fcb6a2e6c
httpjamesm <github@httpjames.space>
2021-04-10 17:07:33 -0400

    remove buggy integrity hashes

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var server = require('http').Server(app)
 var io = require('socket.io')(server);
 const port = 6969
 
+// API Routes
+
 app.get('/', (req, res) => {
     res.sendFile('templates/client_chat.html', { root: __dirname });
 })
@@ -16,9 +18,21 @@ app.get('/manifest.webmanifest', (req, res) => {
     res.sendFile('manifest.webmanifest', { root: __dirname });
 })
 
+// Scripts
+
 app.get('/sw.js', (req, res) => {
     res.sendFile('sw.js', { root: __dirname });
 })
+
+app.get('/aes', (req, res) => {
+    res.sendFile('scripts/aes.js', { root: __dirname })
+})
+
+app.get('/jquery', (req, res) => {
+    res.sendFile('scripts/jquery.min.js')
+})
+
+// Icons
 
 app.get('/icons/512', (req, res) => {
     res.sendFile('icons/cryptochat 512.png', { root: __dirname });
@@ -28,13 +42,18 @@ app.get('/icons/192', (req, res) => {
     res.sendFile('icons/cryptochat 192.png', { root: __dirname });
 })
 
+app.get('/favicon.ico', (req, res) => {
+    res.sendFile('icons/favicon.ico', { root: __dirname });
+})
+
+// Audio
+
 app.get('/notification', (req, res) => {
     res.sendFile('notification.mp3', { root: __dirname });
 })
 
-app.get('/favicon.ico', (req, res) => {
-    res.sendFile('icons/favicon.ico', { root: __dirname });
-})
+
+// SocketIO
 
 io.on('connection', connection =>  {
     connection.on('chat event', data => {
@@ -42,6 +61,8 @@ io.on('connection', connection =>  {
     });
 });
 
+// Server start
+
 server.listen(port, () => {
     console.log('listening on *:' + port);
-  });
+});
