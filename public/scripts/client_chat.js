d097e80ea938071be0bc090d0e9102b8ec59e688
httpjamesm <github@httpjames.space>
2021-04-11 00:40:30 -0400

    cleanup debug stuff, comment javascript

--- public/scripts/client_chat.js
+++ public/scripts/client_chat.js
@@ -6,11 +6,13 @@ function checkCommands() {
     // switch for command
     switch (args[0]) {
         case "/nick":
+            // change username
             if (args.length == 1) {
+                // if no username arguments were provided then alert the user
                 window.alert("Invalid nickname. Correct usage: /nick <username>");
-    
             } else {
                 socket.emit('chat event', {
+                    // broadcast the username change to the whole room
                     user_name : code.encryptMessage(user_name, decryptPass),  
                     message : code.encryptMessage("changed their username to " + args[1], decryptPass)
                 });
@@ -19,14 +21,15 @@ function checkCommands() {
                 var user_name = args[1];
                 window.alert("Nickname changed to " + args[1]);
             }
-    
             break;
             
         case "/leave":
+            // leave the room
             location.reload();
             break;
             
         default:
+            // if no slash command, just send the message normally
             form2();
     }
 }
@@ -46,12 +49,16 @@ if ('serviceWorker' in navigator) {
     });
 }
 
+// ask user for username and key
 var user_name = prompt('Username:', '');
 var decryptPass = prompt('Encryption Key:', '');
+
+// load notification sound
 var notificationsound = new Audio('https://' + window.location.hostname + ':' + location.port + '/sounds/notification.mp3');
-notificationsound.volume = 0.1;
+notificationsound.volume = 0.1; //lower notification volume
 
 let code = (function () {
+    // encryption function
     return {
         encryptMessage: function (messageToencrypt = '', secretkey = '') {
             var encryptedMessage = CryptoJS.AES.encrypt(messageToencrypt, secretkey);
@@ -65,16 +72,19 @@ let code = (function () {
     };
 })();
 
+// connect to socketio endpoint
 var socket = io.connect('https://' + window.location.hostname + ':' + location.port);
 
 socket.on('connect', function () {
+    // on connect
     socket.emit('chat event', {
         data: 'User Connected'  
     });
 });
 
+// focus the message input box
 document.getElementById('msg').focus();
-
+// bind the enter key to checkCommands for the message input box
 document.getElementById('msg').addEventListener('keyup', function (event) {
     event.preventDefault();
     
@@ -83,70 +93,84 @@ document.getElementById('msg').addEventListener('keyup', function (event) {
     }
 });
 
+// change to the key name
 document.getElementById('keyname').innerText = 'Key: ' + decryptPass;
 
 socket.emit('chat event', {
+    // on join, broadcast to room
     user_name : code.encryptMessage(user_name, decryptPass),
     message : code.encryptMessage('has joined the room.', decryptPass)
 });
 
 function form2 () {
-    let user_input =  document.getElementById('msg').value;
+    // send message from message box 
+
+    let user_input =  document.getElementById('msg').value; // get the text from the message input box
     
     if (user_input == '') {
+        // if there's nothing in the box, don't send anything to the server
         return;
     }
     
     socket.emit('chat event', {
+        // encrypt and send the user's name and message
         user_name : code.encryptMessage(user_name, decryptPass),  
         message : code.encryptMessage(user_input, decryptPass)
     });
     
-    $('input.message').val('').focus();
+    $('input.message').val('').focus(); // clear the message input box after send
 }
 
 socket.on('my response', function (msg) {
-    console.log(msg);
+    console.log(msg); // for debugging: print the encrypted contents of the response
     
     if (typeof msg.user_name !== 'undefined') {
-        $('h3').remove();
+        // create a paragraph element and name it 'messagetxt'
         messagebox = document.createElement('P');
         messagebox.className = 'messagetxt';
         
         if (code.decryptMessage(msg.user_name, decryptPass) == '' && code.decryptMessage(msg.message, decryptPass) == '') {
+            // if the username and key are empty values, stop
             return;
         }
         
+        // create a text node with the decrypted username and message
         text = document.createTextNode(code.decryptMessage(msg.user_name, decryptPass) + ': ' + code.decryptMessage(msg.message, decryptPass));
-        messagebox.appendChild(text);
-        messages = document.getElementsByName('messageviewer')[0];
-        messages.appendChild(messagebox);
-        messages.scrollTop = messages.scrollHeight;
+        messagebox.appendChild(text); // append the node to the p element
+        messages = document.getElementsByName('messageviewer')[0]; // get the messageviewer object
+        messages.appendChild(messagebox); // append the p element to the messageviewer object
+        messages.scrollTop = messages.scrollHeight; // auto-scroll the message viewer for convenience
         
         if (document.hasFocus()) {
+            // if the tab is in focus, don't play any notification sound
             return;
         }
         
-        notificationsound.play();
+        notificationsound.play(); // play a notification sound
     }
 });
 
 function switchTheme() {
-    button = document.getElementById('toggler').innerText;
-    console.log(button);
+    // switch from dark to light and vice-versa
+
+    button = document.getElementById('toggler').innerText; // get the text value of the theme button
     
     if (button === 'DARK') {
-        console.log('yes');
+        // if the button says 'dark', switch the page to light theme
+
         document.documentElement.setAttribute('data-theme','light');
         document.getElementById('toggler').innerText = 'LIGHT';
-        
     } else {
+        // otherwise, just switch the page to dark theme
+
         document.documentElement.setAttribute('data-theme','dark');
         document.getElementById('toggler').innerText = 'DARK';
     }
 }
 
 function leaveRoom() {
+    // on tab close, broadcast to the room
+
     socket.emit('chat event', {
         user_name : code.encryptMessage(user_name, decryptPass),
         message : code.encryptMessage('has left the room.', decryptPass)
@@ -154,6 +178,8 @@ function leaveRoom() {
 }
 
 window.addEventListener('beforeunload', function (evt) {
+    // execute code before the tab closes
+    
     // Cancel the event (if necessary)
     evt.preventDefault();
     // Google Chrome requires returnValue to be set
@@ -161,6 +187,7 @@ window.addEventListener('beforeunload', function (evt) {
     evt.returnValue = '';
 });
 
-
+// bind the send button to checkCommands()
 document.getElementById("sendbutton").addEventListener("click", checkCommands);
+// bind the theme toggle button to switchTheme()
 document.getElementById("toggler").addEventListener("click", switchTheme);
--- public/scripts/legal.js
+++ public/scripts/legal.js
@@ -1,17 +1,21 @@
 function switchTheme() {
-    button = document.getElementById('toggler').innerText;
-    console.log(button);
+    // switch from dark to light and vice-versa
+
+    button = document.getElementById('toggler').innerText; // get the text value of the theme button
     
-    if (button === 'DARK'){
-        console.log('yes');
+    if (button === 'DARK') {
+        // if the button says 'dark', switch the page to light theme
+
         document.documentElement.setAttribute('data-theme','light');
         document.getElementById('toggler').innerText = 'LIGHT';
-        
     } else {
+        // otherwise, just switch the page to dark theme
+
         document.documentElement.setAttribute('data-theme','dark');
         document.getElementById('toggler').innerText = 'DARK';
     }
 }
 
+// bind the theme toggle button to switchTheme()
 document.getElementById("toggler").addEventListener("click", switchTheme);
 
