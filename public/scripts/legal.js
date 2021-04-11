1b94bef549bb7db2e0de414f9f20f42dc1df912a
httpjamesm <github@httpjames.space>
2021-04-10 23:57:48 -0400

    remove csp, nginx header has been added

---
+++ public/scripts/legal.js
@@ -0,0 +1,14 @@
+function switchTheme(e) {
+    button = document.getElementById('toggler').innerText;
+    console.log(button);
+    
+    if (button === 'DARK'){
+        console.log('yes');
+        document.documentElement.setAttribute('data-theme','light');
+        document.getElementById('toggler').innerText = 'LIGHT';
+        
+    } else {
+        document.documentElement.setAttribute('data-theme','dark');
+        document.getElementById('toggler').innerText = 'DARK';
+    }
+}
