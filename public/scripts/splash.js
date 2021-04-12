abbf9d6d61f2c712f0c5e1962df252f90f45d78a
httpjamesm <github@httpjames.space>
2021-04-12 14:21:11 -0400

    add service worker to splash

--- public/scripts/splash.js
+++ public/scripts/splash.js
@@ -1,3 +1,23 @@
+// Registering our Service worker
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/sw.js').then(
+            function (registration) {
+                // Registration was successful
+                console.log(
+                    'ServiceWorker registration successful with scope: ',
+                    registration.scope
+                );
+            },
+            function (err) {
+                // Registration Failed :(
+                console.log('ServiceWorker registration failed: ', err);
+            }
+        );
+    });
+}
+
 function switchTheme() {
     // switch from dark to light and vice-versa
 
