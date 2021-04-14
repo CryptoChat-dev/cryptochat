a7a13b828429f6b6f16cd04700462852057872e9
httpjamesm <github@httpjames.space>
2021-04-13 20:15:48 -0400

    fix back slash bug in /shrug

--- public/scripts/client_chat.js
+++ public/scripts/client_chat.js
@@ -45,10 +45,11 @@ function checkCommands() {
         case '/shrug':
             socket.emit('chat event', {
                 "user_name": code.encryptMessage(user_name, decryptPass),
-                "message": code.encryptMessage("¯\_(ツ)_/¯", decryptPass)
+                "message": code.encryptMessage("¯\\_(ツ)_/¯", decryptPass)
             })
+            $('input.message').val('').focus();
             break;
-            
+
         default:
             // if no slash command, just send the message normally
             form2();
